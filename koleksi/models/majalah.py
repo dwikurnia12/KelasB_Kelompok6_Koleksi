440144810333bb6a83e97f80c2605f7daec80efd
queenneramartha17-coder <queenneramartha17@student.uns.ac.id>
2026-06-02 09:01:08 +0700

    majalah

---
+++ koleksi/models/majalah.py
@@ -0,0 +1,17 @@
+from koleksi.models.koleksi import Koleksi
+
+class Majalah(Koleksi):
+
+    def __init__(self, kode_koleksi: str, judul: str, tahun_terbit: str,
+                 penerbit: str, edisi: str):
+        super().__init__(kode_koleksi, judul, tahun_terbit, penerbit)
+        self._edisi = edisi
+
+    def tampilkan_info(self) -> None:
+
+        print(f"Jenis          : Majalah")
+        print(f"Kode Koleksi   : {self._kode_koleksi}")
+        print(f"Judul          : {self._judul}")
+        print(f"Thn Terbit     : {self._tahun_terbit}")
+        print(f"Penerbit       : {self._penerbit}")
+        print(f"Edisi          : {self._edisi}")
